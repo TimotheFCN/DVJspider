28792e76829db3eedba3e25a03816f62ceb1de91
Timothé FOCONE <timothe.focone@edu.devinci.fr>
2021-04-20 01:10:10 +0200

    Added SQL offers logging to only show new ones

--- graphistesonline.js
+++ graphistesonline.js
@@ -1,10 +1,11 @@
-require('dotenv').config({path: __dirname + '/.env'})
+require('dotenv').config({path: __dirname + '/.env'});
 
 //Login infos from the confg file
 let username = process.env.GO_Username;
 let password = process.env.GO_Password;
 
-exports.run = async function(page) {
+//Check new offers and sends a message on Discord if so
+exports.run = async function(page, db) {
     const mainurl = 'https://www.graphistesonline.com/mypage.php?quoi=my_demandes4';
     await page.goto(mainurl);
     await page.waitForSelector('#loader', {visible: false}); //Wait for the page to load
@@ -13,9 +14,10 @@ exports.run = async function(page) {
     if(page.url() !== mainurl) await login(page); //Log in
 
     await page.goto(mainurl);
-    await getOffers(page);
+    await processOffers(db, await getOffers(page));
 }
 
+//Log with a specified user on the site
 async function login(page) {
     await page.goto('https://www.graphistesonline.com/visitor_mypage_3.php?quoi=login&where='); //Go to the login page
     await page.waitForSelector('#loader', {visible: false}); //Wait for the page to load
@@ -25,6 +27,7 @@ async function login(page) {
     await page.click('#form_submit', { delay: 20 })
 }
 
+//Sends back all offers on the first page
 async function getOffers(page) {
     await page.waitForSelector('//*[@id="liste_all"]/ul/li[1]/a/h3', {visible: true}, {delay : 2000}); //Wait for the page to load
 
@@ -59,5 +62,18 @@ async function getOffers(page) {
       }
       //Return the array
       return offers;
+}
 
+//Check for each offers if it is already in the database. If not, sends a message on Discord and updates the database.
+async function processOffers(db, offers) {
+    offers.forEach(offer => {
+        db.all('SELECT 1 FROM GraphistesOnline WHERE id = ' + offer.num, (err, result) => {
+            if(err) throw err;
+            if (result.length == 0) //Si l'id n'existe pas déjà
+            {
+                db.run('INSERT INTO GraphistesOnline VALUES(?)', [offer.num]); //Ajout de l'ID à la base
+                console.log("Nouvelle offre trouvée : " + offer.num);
+            }
+        });
+    });
 }
--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-require('dotenv').config({path: __dirname + '/.env'})
+require('dotenv').config({path: __dirname + '/.env'});
 const playwright = require('playwright');
+const sqlite3 = require('sqlite3');
 
 const graphistesonline = require('./graphistesonline')
 
+//Opening sqlite database to store websites last updates
+let db = new sqlite3.Database('updates.db', err => {
+  if(err) throw err;
+  else console.log("Database opened on updates.db");
+})
 
 module.exports.run = async() => {
-    const browser = await playwright.chromium.launch({headless: false}); //Open a new brower window
-    const page = await browser.newPage(); //Open a new page
-    //Block useless content on the page
-    await page.route('**/*', route => {
-        if(route.request().resourceType() === 'image' || route.request().resourceType() === 'css' || route.request().url().includes("livechatinc")|| route.request().url().includes("google"))
-            route.abort()
-        else route.continue();
+    
+  //Create tables if they don't exists
+  db.run(`CREATE TABLE IF NOT EXISTS GraphistesOnline(id INTEGER PRIMARY KEY)`);
+  
+  const browser = await playwright.chromium.launch({headless: false}); //Open a new brower window
+  const page = await browser.newPage(); //Open a new page
+    
+  //Block useless content on the page
+  await page.route('**/*', route => {
+      if(route.request().resourceType() === 'image' || route.request().resourceType() === 'css' || route.request().url().includes("livechatinc")|| route.request().url().includes("google"))
+          route.abort()
+       else route.continue();
       });
 
     //Run all websites checks
-    await graphistesonline.run(page); //Refresh Graphistesonline
+    await graphistesonline.run(page, db); //Refresh Graphistesonline
 
 
 
 
     browser.close();
+    db.close();
 }
 
 
