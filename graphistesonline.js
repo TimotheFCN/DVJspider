df0bab54a49af733d2b4fbdc2786cc04c9a2925b
Timothé Focone <timo.2001@hotmail.fr>
2021-06-04 15:49:56 +0200

    [Working version] Added support and notifications for the feedback page

--- graphistesonline.js
+++ graphistesonline.js
@@ -1,10 +1,11 @@
-require('dotenv').config({path: __dirname + '/.env'});
+require('dotenv').config({ path: __dirname + '/.env' });
 const Discord = require('discord.js');
 
 //Login infos and discord channel from the confg file
 let username = process.env.GO_Username;
 let password = process.env.GO_Password;
 var channel;
+var messageSent = false; //If feedback message has already been sent
 
 //Check new offers and sends a message on Discord if so
 exports.run = async function(page, db, client) {
@@ -15,65 +16,83 @@ exports.run = async function(page, db, client) {
     //await page.waitForSelector('#loader', {visible: false}); //Wait for the page to load
 
     //Check if the user is logged, if not, log the user
-    if(page.url() !== mainurl) await login(page); //Log in
+    if (page.url().includes("login")) await login(page); //Log in
 
     await page.goto(mainurl);
-    await processOffers(db, await getOffers(page));
+    await page.waitForSelector('#bloc-presta-prj-dispo', { visible: false }); //Wait for the page to load
+
+    if (page.url().includes("feedback")) EvaluateMessage();
+    else {
+        messageSent = false;
+        await processOffers(db, await getOffers(page));
+    }
 }
 
 //Log with a specified user on the site
 async function login(page) {
     console.log("login");
     await page.goto('https://www.graphistesonline.com/visitor_mypage_3.php?quoi=login&where='); //Go to the login page
-    await page.waitForSelector('#loader', {visible: false}); //Wait for the page to load
+    await page.waitForSelector('#loader', { visible: false }); //Wait for the page to load
     //Fill the form
     await page.type('input[name=nick]', username, { delay: 20 });
     await page.type('input[name=password]', password, { delay: 20 });
+    await page.waitForSelector('#form_submit', { delay: 20 });
     await page.click('#form_submit', { delay: 20 })
+    return;
+}
+
+//Check if a feedback request is pending
+async function EvaluateMessage(page) {
+    console.log("feedback pending");
+    if (!messageSent) {
+        channel.send("\n @here\n /!\\  `Une demande de feedback est en attente, impossible de vérifier les nouvelles offres.`  /!\\ \n<https://www.graphistesonline.com/mypage.php?quoi=my_demandes4>");
+        messageSent = true;
+    }
+    return;
 }
 
 //Sends back all offers on the first page
 async function getOffers(page) {
-    await page.waitForSelector('//*[@id="liste_all"]/ul/li[1]/a/h3', {visible: true}, {delay : 2000}); //Wait for the page to load
+    await page.waitForSelector('//*[@id="liste_all"]/ul/li[1]/a/h3', { visible: true }, { delay: 2000 }); //Wait for the page to load
 
     //Select the list of offers and put it in the array "result"
-    let listSelector="#liste_all > ul > li > a";
+    let listSelector = "#liste_all > ul > li > a";
     var result = await page.$$eval(listSelector, list => {
         var result = [];
         for (let i = 0; i < list.length; i++) {
-          var data = {
-            href: list[i].href,
-            textContent: list[i].textContent,
-            innerHTML: list[i].innerHTML
-          };
-          result.push(data);
+            var data = {
+                href: list[i].href,
+                textContent: list[i].textContent,
+                innerHTML: list[i].innerHTML
+            };
+            result.push(data);
         }
         return result;
-      });
+    });
 
 
-      //Formatting the array with the desired infos (offers contains a list of offer)
-      var offers = [];
-      for (let element of result) {
-          let offer = {
-              //Use the string formatting to get the infos we need
-              link: element.href, //Link to the offer
-              num: element.href.split("&id=")[1], //ID of the offer
-              title: (element.innerHTML.substring( element.innerHTML.indexOf("&nbsp;") + 6, element.innerHTML.indexOf("("+element.href.split("&id=")[1]+")")).replaceAll("&nbsp;", "")+" "), //Title of the offer
-              desc: element.innerHTML.substring( element.innerHTML.indexOf("projet : </strong>") + 18, element.innerHTML.indexOf("<br><br>")) //Description of the offer
+    //Formatting the array with the desired infos (offers contains a list of offer)
+    var offers = [];
+    for (let element of result) {
+        let offer = {
+            //Use the string formatting to get the infos we need
+            link: element.href, //Link to the offer
+            num: element.href.split("&id=")[1], //ID of the offer
+            title: (element.innerHTML.substring(element.innerHTML.indexOf("&nbsp;") + 6, element.innerHTML.indexOf("(" + element.href.split("&id=")[1] + ")")).replace("&nbsp;", "") + " "), //Title of the offer
+            desc: element.innerHTML.substring(element.innerHTML.indexOf("projet : </strong>") + 18, element.innerHTML.indexOf("<br><br>")) //Description of the offer
 
-          }
-          offers.push(offer);
-      }
-      //Return the array
-      return offers;
+        }
+        offers.push(offer);
+    }
+    //Return the array
+    return offers;
 }
 
 //Check for each offers if it is already in the database. If not, sends a message on Discord and updates the database.
 async function processOffers(db, offers) {
     offers.forEach(offer => {
         db.all('SELECT 1 FROM GraphistesOnline WHERE id = ' + offer.num, (err, result) => {
-            if(err) throw err;
+            if (err) throw err;
             if (result.length == 0) //If the ID doesn't exist
             {
                 sendMessage(offer);
@@ -91,31 +110,30 @@ async function sendMessage(offer) {
         "url": offer.link,
         "color": 13570130,
         "thumbnail": {
-          "url": "https://pbs.twimg.com/profile_images/1241262105/logo_graphistesonline_400x400.PNG"
+            "url": "https://pbs.twimg.com/profile_images/1241262105/logo_graphistesonline_400x400.PNG"
         },
         "author": {
-          "name": "GraphistesOnline",
-          "url": "https://graphistesonline.com",
-          "icon_url": "https://pbs.twimg.com/profile_images/1241262105/logo_graphistesonline_400x400.PNG"
+            "name": "GraphistesOnline",
+            "url": "https://graphistesonline.com",
+            "icon_url": "https://pbs.twimg.com/profile_images/1241262105/logo_graphistesonline_400x400.PNG"
         },
-        "fields": [
-          {
-            "name": "Titre",
-            "value": offer.title
-          },
-          {
-            "name": "Description",
-            "value": offer.desc
-          },
-          {
-            "name": "​",
-            "value": "​"
-          },
-          {
-            "name": "👇🏻",
-            "value": offer.link
-          }
+        "fields": [{
+                "name": "Titre",
+                "value": offer.title
+            },
+            {
+                "name": "Description",
+                "value": offer.desc
+            },
+            {
+                "name": "​",
+                "value": "​"
+            },
+            {
+                "name": "👇🏻",
+                "value": offer.link
+            }
         ]
-      };
-      channel.send("\n**Un nouvel appel d'offre a été publié !** \n@here", { embed });
+    };
+    channel.send("\n\n**Un nouvel appel d'offre a été publié !** \n@here", { embed });
 }
--- index.js
+++ index.js
@@ -1,46 +1,47 @@
-require('dotenv').config({path: __dirname + '/.env'});
+require('dotenv').config({ path: __dirname + '/.env' });
 const playwright = require('playwright');
 const sqlite3 = require('sqlite3');
 const Discord = require('discord.js');
+var botLogged = false;
 
 const graphistesonline = require('./graphistesonline');
 const client = new Discord.Client();
 
 //Opening sqlite database to store websites last updates
 let db = new sqlite3.Database('updates.db', err => {
-  if(err) throw err;
-  else console.log("Database opened on updates.db");
+    if (err) throw err;
+    else console.log("Database opened on updates.db");
 })
 
 module.exports.run = async() => {
-    
-  //Create tables if they don't exists
-  db.run(`CREATE TABLE IF NOT EXISTS GraphistesOnline(id INTEGER PRIMARY KEY)`);
 
-  //DISCORD LOGIN
-  await client.login(process.env.Discord_Token) //Login with Discord API
-  await client.on('ready', function() {
-      console.log("Bot connected"); // Wait for the bot to connect
-  })
+    //Create tables if they don't exists
+    db.run(`CREATE TABLE IF NOT EXISTS GraphistesOnline(id INTEGER PRIMARY KEY)`);
 
-  const browser = await playwright.chromium.launch({headless: true}); //Open a new brower window
-  const page = await browser.newPage(); //Open a new page
-    
-  //Block useless content on the page
-  await page.route('**/*', route => {
-      if(route.request().resourceType() === 'image' || route.request().resourceType() === 'css' || route.request().url().includes("livechatinc")|| route.request().url().includes("google"))
-          route.abort()
-       else route.continue();
-      });
+    //DISCORD LOGIN
+    await client.login(process.env.Discord_Token); //Login with Discord API
+    client.on('ready', function() {
+        botLogged = true;
+        console.log("Bot connected");
+    })
+
+    const browser = await playwright.chromium.launch( /*{ headless: false }*/ ); //Open a new brower window
+    const page = await browser.newPage(); //Open a new page
+
+    //Block useless content on the page
+    await page.route('**/*', route => {
+        if (route.request().resourceType() === 'image' || route.request().resourceType() === 'css' || route.request().url().includes("livechatinc") || route.request().url().includes("google"))
+            route.abort()
+        else route.continue();
+    });
 
 
     //Run all websites checks every 5 minutes
     setInterval(async function() {
-      console.log("Checking websites");
-      await graphistesonline.run(page, db, client); //Refresh Graphistesonline
-    }, 30000);
+        console.log("Checking websites");
+        await graphistesonline.run(page, db, client); //Refresh Graphistesonline
+    }, 300000);
 
 }
-
 
 module.exports.run();
