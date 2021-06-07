371ffe5a7299d1f3a1a26d28242409e790a36bfb
Timothé Focone <timo.2001@hotmail.fr>
2021-06-07 12:21:30 +0200

    Added Kiwi, but it doesn't work rip

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Discord = require('discord.js');
 var botLogged = false;
 
 const graphistesonline = require('./graphistesonline');
+const kiwi = require('./kiwi');
 const client = new Discord.Client();
 
 //Opening sqlite database to store websites last updates
@@ -17,6 +18,7 @@ module.exports.run = async() => {
 
     //Create tables if they don't exists
     db.run(`CREATE TABLE IF NOT EXISTS GraphistesOnline(id INTEGER PRIMARY KEY)`);
+    db.run(`CREATE TABLE IF NOT EXISTS Kiwi(id INTEGER PRIMARY KEY)`);
 
     //DISCORD LOGIN
     await client.login(process.env.Discord_Token); //Login with Discord API
@@ -25,19 +27,26 @@ module.exports.run = async() => {
         console.log("Bot connected");
     })
 
-    const browser = await playwright.chromium.launch( /*{ headless: false }*/ ); //Open a new brower window
-    const page = await browser.newPage(); //Open a new page
+    const browser = await playwright.chromium.launch({ headless: false }); //Open a new brower window
+    const page_gonline = await browser.newPage(); //Open a new page for GraphistesOnline
+    const page_kiwi = await browser.newPage(); //Open a new page for Kiwi
 
     //Block useless content on the page
-    await page.route('**/*', route => {
+    await page_gonline.route('**/*', route => {
         if (route.request().resourceType() === 'image' || route.request().resourceType() === 'css' || route.request().url().includes("livechatinc") || route.request().url().includes("google"))
+            route.abort()
+        else route.continue();
+    });
+    await page_kiwi.route('**/*', route => {
+        if (route.request().resourceType() === 'image' || route.request().resourceType() === 'css')
             route.abort()
         else route.continue();
     });
 
     async function refreshSites() {
         console.log("Checking websites");
-        await graphistesonline.run(page, db, client); //Refresh Graphistesonline
+        //await graphistesonline.run(page_gonline, db, client); //Refresh Graphistesonline
+        await kiwi.run(page_kiwi, db, client); //Refresh Graphistesonline
     }
 
     await refreshSites();
---
+++ kiwi.js
@@ -0,0 +1,132 @@
+require('dotenv').config({ path: __dirname + '/.env' });
+const Discord = require('discord.js');
+
+//Login infos and discord channel from the confg file
+let username = process.env.KIWI_Username;
+let password = process.env.KIWI_Password;
+var channel;
+
+//Check new offers and sends a message on Discord if so
+exports.run = async function(page, db, client) {
+    channel = client.channels.cache.get(process.env.Discord_Channel);
+
+    const mainurl = 'https://kiwi.junior-entreprises.com/business/appels-d-offres/';
+    await page.goto(mainurl);
+    setTimeout(() => {}, 2000);
+
+    //Check if the user is logged, if not, log the user
+    if (page.url().includes("auth")) {
+        await login(page); //Log in
+        console.log("logged");
+    }
+
+    await page.goto(mainurl);
+    await page.waitForSelector('body > app-root > div > div.mybody > app-navbar > mat-sidenav-container > mat-sidenav-content > div > div > app-appels-d-offres > div > div:nth-child(4) > div:nth-child(2)', { visible: false }); //Wait for the page to load
+    setTimeout(() => {}, 2000);
+    //await processOffers(db, await getOffers(page));
+    await getOffers(page);
+}
+
+//Log with a specified user on the site
+async function login(page) {
+    console.log("login");
+    await page.goto('https://auth.junior-entreprises.com/?returnUrl=https:%2F%2Fkiwi.junior-entreprises.com%2Flogin'); //Go to the login page
+    await page.waitForSelector('body > app-root > app-login > div.container > div > mat-card > mat-card-actions > button'); //Wait for the page to load
+    //Fill the form
+    await page.type('#mat-input-0', username, { delay: 20 });
+    await page.type('#mat-input-1', password, { delay: 20 });
+    await page.keyboard.press('Enter');
+    //await page.click('body > app-root > app-login > div.container > div > mat-card > mat-card-actions > button', { delay: 5000 })
+    setTimeout(() => {}, 1000);
+    await page.waitForSelector('body > app-root > div > div.mybody > app-navbar > mat-sidenav-container > mat-sidenav-content > div > div > app-home > div > h1');
+    return;
+}
+
+//Sends back all offers on the first page
+async function getOffers(page) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    await page.waitForSelector('body > app-root > div > div.mybody > app-navbar > mat-sidenav-container > mat-sidenav-content > div > div > app-appels-d-offres > div > div:nth-child(4) > div:nth-child(2)', { visible: true }); //Wait for the page to load
+    //Select the list of offers and put it in the array "result"
+    let listSelector = '[class="mat-button-wrapper"]';
+    console.log(await page.$eval(listSelector).innerHTML);
+    var result = await page.$$eval(listSelector, list => {
+        var result = [];
+        for (let i = 0; i < list.length; i++) {
+            var data = {
+                href: list[i].href,
+                textContent: list[i].textContent,
+                innerHTML: list[i].innerHTML
+            };
+            result.push(data);
+        }
+        return result;
+    });
+
+
+    //Formatting the array with the desired infos (offers contains a list of offer)
+    var offers = [];
+    for (let element of result) {
+
+        let offer = {
+            //Use the string formatting to get the infos we need
+            link: element.href, //Link to the offer
+            num: element.href.split("&id=")[1], //ID of the offer
+            title: (element.innerHTML.substring(element.innerHTML.indexOf("&nbsp;") + 6, element.innerHTML.indexOf("(" + element.href.split("&id=")[1] + ")")).replace("&nbsp;", "") + " "), //Title of the offer
+            desc: element.innerHTML.substring(element.innerHTML.indexOf("projet : </strong>") + 18, element.innerHTML.indexOf("<br><br>")) //Description of the offer
+
+        }
+        offers.push(offer);
+    }
+    //Return the array
+    return offers;
+}
+
+//Check for each offers if it is already in the database. If not, sends a message on Discord and updates the database.
+async function processOffers(db, offers) {
+    offers.forEach(offer => {
+        db.all('SELECT 1 FROM Kiwi WHERE id = ' + offer.num, (err, result) => {
+            if (err) throw err;
+            if (result.length == 0) //If the ID doesn't exist
+            {
+                sendMessage(offer);
+                db.run('INSERT INTO Kiwi VALUES(?)', [offer.num]); //Add the ID to the database
+                console.log("New offer found : " + offer.num);
+            }
+        });
+    });
+}
+
+//Build and send the message to Discord
+async function sendMessage(offer) {
+    const embed = {
+        "title": offer.num,
+        "url": offer.link,
+        "color": 13570130,
+        "thumbnail": {
+            "url": "https://pbs.twimg.com/profile_images/1241262105/logo_graphistesonline_400x400.PNG"
+        },
+        "author": {
+            "name": "GraphistesOnline",
+            "url": "https://graphistesonline.com",
+            "icon_url": "https://pbs.twimg.com/profile_images/1241262105/logo_graphistesonline_400x400.PNG"
+        },
+        "fields": [{
+                "name": "Titre",
+                "value": offer.title
+            },
+            {
+                "name": "Description",
+                "value": offer.desc
+            },
+            {
+                "name": "​",
+                "value": "​"
+            },
+            {
+                "name": "👇🏻",
+                "value": offer.link
+            }
+        ]
+    };
+    channel.send("\n\n**Un nouvel appel d'offre a été publié !** \n@here", { embed });
+}
